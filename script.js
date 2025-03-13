2b386d1e2a869ece3ec91a82754adc17b825bcc1
Byeolyyy <1371707335@qq.com>
2025-03-13 20:39:29 +0800

    Fix: Return to index.html after each question

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const questions = [
-    { text: "改成文字版的就OK 不用图画的", options: ["A", "B", "C", "D"], answer: 0 },
+    { text: "结合分析，小美是Bigbang中谁的粉丝? 小美：今年是喜欢bigbang的第5年，我是他的梦女，我非常喜欢他在舞台上的舞姿和穿搭。", options: ["A", "B", "C", "D"], answer: 0 },
     { text: "郑雅娴最像什么动物？", options: ["老鼠", "猫", "熊", "鸭子", "松鼠"], answer: 1 },
     { text: "我的英文名叫什么？", options: ["Zavier", "Helen", "Olivia", "Eva", "Jay"], answer: 3 },
     { text: "要不要永远跟我做好朋友？", options: ["要", "不要"], answer: 0 }
@@ -38,7 +38,7 @@ function checkAnswer(selectedIndex, button) {
             if (level === questions.length - 1) {
                 window.location.href = "youwin.html";
             } else {
-                window.location.href = `home.html?unlocked=${level + 2}`;
+                window.location.href = `index.html?unlocked=${level + 2}`;
             }
         }, 1000);
     } else {
